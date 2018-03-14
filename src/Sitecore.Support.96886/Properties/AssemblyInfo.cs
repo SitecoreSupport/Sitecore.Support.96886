192f7fe4b2d8e476c4b0cb46258431a3961ebc93
sc-ido <ido@sitecore.net>
2018-03-14 11:07:22 +0200

    Original method

---
+++ src/Sitecore.Support.96886/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.96886")]
+[assembly: AssemblyProduct("Sitecore.Support.96886")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.96886/Resources/Media/MediaProvider.cs
@@ -0,0 +1,90 @@
+﻿using Sitecore.Configuration;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.IO;
+using Sitecore.Resources;
+using Sitecore.Resources.Media;
+using Sitecore.Web;
+using System;
+
+namespace Sitecore.Support.Resources.Media
+{
+  public class MediaProvider : Sitecore.Resources.Media.MediaProvider
+  {
+    /// <summary>
+    /// Gets a media URL.
+    /// </summary>
+    /// <param name="item">The media item.</param>
+    /// <param name="options">The query string.</param>
+    /// <returns>The media URL.</returns>
+    [NotNull]
+    public override string GetMediaUrl([NotNull] MediaItem item, [NotNull] MediaUrlOptions options)
+    {
+      Assert.ArgumentNotNull(item, "item");
+      Assert.ArgumentNotNull(options, "options");
+
+      bool hasMediaContent = options.Thumbnail || this.HasMediaContent(item);
+
+      if (!hasMediaContent && item.InnerItem["path"].Length > 0)
+      {
+        return options.LowercaseUrls ? item.InnerItem["path"].ToLowerInvariant() : item.InnerItem["path"];
+      }
+
+      if (options.UseDefaultIcon && !hasMediaContent)
+      {
+        return options.LowercaseUrls ? Themes.MapTheme(Settings.DefaultIcon).ToLowerInvariant() : Themes.MapTheme(Settings.DefaultIcon);
+      }
+
+      Assert.IsTrue(this.Config.MediaPrefixes[0].Length > 0, "media prefixes are not configured properly.");
+      string prefix = this.MediaLinkPrefix;
+
+      if (options.AbsolutePath)
+      {
+        prefix = options.VirtualFolder + prefix;
+      }
+      else if (prefix.StartsWith("/", StringComparison.InvariantCulture))
+      {
+        prefix = StringUtil.Mid(prefix, 1);
+      }
+
+      prefix = MainUtil.EncodePath(prefix, '/');
+
+      if (options.AlwaysIncludeServerUrl)
+      {
+        prefix = FileUtil.MakePath(string.IsNullOrEmpty(options.MediaLinkServerUrl) ? WebUtil.GetServerUrl() : options.MediaLinkServerUrl, prefix, '/');
+      }
+
+      string extension = StringUtil.GetString(options.RequestExtension, item.Extension, Constants.AshxExtension);
+
+      extension = StringUtil.EnsurePrefix('.', extension);
+
+      string parameters = options.ToString();
+
+      if (parameters.Length > 0)
+      {
+        extension += "?" + parameters;
+      }
+
+      string mediaRoot = Constants.MediaLibraryPath + "/";
+      string itemPath = item.InnerItem.Paths.Path;
+
+      string path;
+
+      // Check whether an item is under media library, 
+      // otherwise it would be hard to define whether it's relative path to an item or absolute
+      if (options.UseItemPath
+        && itemPath.StartsWith(mediaRoot, StringComparison.OrdinalIgnoreCase))
+      {
+        path = StringUtil.Mid(itemPath, mediaRoot.Length);
+      }
+      else
+      {
+        path = item.ID.ToShortID().ToString();
+      }
+
+      path = MainUtil.EncodePath(path, '/');
+      path = prefix + path + (options.IncludeExtension ? extension : string.Empty);
+      return options.LowercaseUrls ? path.ToLowerInvariant() : path;
+    }
+  }
+}
